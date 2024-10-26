b6f1a73e53d3cb7d76f07e6e44bd4cf267b987b0
nawa316 <muhammadadedzakwan@gmail.com>
2024-10-26 09:40:30 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <section className="flex flex-col lg:flex-row">
+      <div className="flex basis-1/2">
+        <img
+          src="./assets/side-image.png"
+          className="flex object-cover w-full lg:h-full h-56"
+          alt="ini gambar hape dan keranjang"
+        />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
 
-export default App
+      <div className="p-8 flex flex-col gap-5 justify-center lg:basis-1/2">
+        <div className="flex flex-col">
+          <h1 className="text-3xl font-medium">Log in to Exclusive</h1>
+          <p>Enter your details below</p>
+        </div>
+
+        <div className="flex flex-col gap-4">
+          <input type="text" placeholder="Email or Phone Number" />
+          <input type="password" placeholder="Password"/>
+        </div>
+
+        <div className="flex flex-row items-center justify-between">
+          <button className="bg-red-600 px-6 py-2 text-neutral-50">Log in</button>
+          <p className="text-red-600">Forget password</p>
+        </div>
+      </div>
+    </section>
+  );
+}
